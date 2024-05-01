131df184c73aff679b5877f067e4f290618ce3f8
David Sinclair <aim_here2002@yahoo.co.uk>
2024-05-01 17:13:33 +0100

    Sync

--- examples/makemodels.py
+++ examples/makemodels.py
@@ -17,14 +17,23 @@ for i, c in enumerate(['x', 'y', 'z']):
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--h36m', action = 'store_true')
 parser.add_argument('infile', type = str)
 parser.add_argument('joint', type = str)
 
 args = parser.parse_args()
 
-outcust_template = 'zed34_out_%s%s'
-outfile_template = './data/models/zed34_out_%s%s.json'
-
+if (args.h36m):
+    outcust_template = 'h36m_out_%s%s'
+    outfile_template = './data/models/h36m_out_%s%s.json'
+    tposefile = './data/target/h36m_S1_tpose.bvh'
+    outputdir = './output_h36mtest'
+else:
+    outcust_template = 'zed34_out_%s%s'
+    outfile_template = './data/models/zed34_out_%s%s.json'
+    tposefile = './data/target/zed_34_tpose_flipped.bvh'
+    outputdir = './output_zedtest'
+    
 with open(args.infile, 'r') as fp:
     js = json.load(fp)
 
@@ -40,9 +49,8 @@ for xv in sorted(coords):
     for yv in sorted(coords):            
         call(['python',
             './run_retargeting.py',
-              './data/target/zed_34_tpose_flipped.bvh',
+              tposefile,
               outcust_template%(xv, yv),
               'data/testsrc',
               'custom',
-              'output/zedtest'])
-                  
+              outputdir])

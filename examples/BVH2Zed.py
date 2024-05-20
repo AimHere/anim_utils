048123ebdb5adae8f3e226d42fb7198eddfa5962
David Sinclair <aim_here2002@yahoo.co.uk>
2024-05-21 00:23:41 +0100

    Working Converter

--- examples/BVH2Zed.py
+++ examples/BVH2Zed.py
@@ -259,40 +259,41 @@ body38tree = {
 #    "RightHeel"
 
 tpose34_pos = [[0,0,0],
-               [-0.000732270938924443,175.158289701814,0.0000404],
-               [0.104501423707752,350.306093180137,0.061023624087894],
-               [0.209734388920577,525.459141360196,0.122005361332611],
-               [-47.5920764358155,526.439401661188,0.945479045649915],
-               [-173.508163386225,526.509589382348,2.98848444604651],
-               [-413.490495136004,529.070006884027,4.30015451481757],
-               [-644.259234923473,531.557805055099,5.5156119864156],
-               [-690.412981869986,532.055366973068,5.75870846651414],
-               [-782.7204827236,533.050484095638,6.24488492674942],
-               [-737.171470366004,477.177940319642,6.03507728101326],
-               [48.0126939292815,526.382576868867,-0.700813930486266],
-               [173.928779031094,526.312389136708,-2.74381818341968],
-               [413.965281719423,525.913766951398,-5.58751914341834],
-               [644.770225962532,525.03114718918,-8.36370311424063],
-               [690.931216942206,524.854620798345,-8.91894225928869],
-               [783.253196286002,524.501573309859,-10.029413939978],
-               [736.897241876494,469.296359031723,-9.22217198040476],
-               [-97.2538992002065,0,-0.0216466824273884],
-               [-97.2534603765872,-398.665678321646,-0.0280368622539885],
-               [-97.236806268837,-753.034804644565,-0.0408179870540185],
-               [-97.2541801180481,-841.630928132314,106.266711069826],
-               [97.2538982249762,0,0.0216480069085377],
-               [97.2606469820357,-398.664829041876,0.0265545153545871],
-               [97.2769776133806,-753.033961966181,0.0252733646276821],
-               [97.2596464463626,-841.626635633415,106.335670500348],
-               [1.30731388292955,660.085767955841,62.6295143653575],
-               [1.37823874062583,704.938429698234,62.5519366785149],
-               [-25.8996591150869,736.342683178085,31.5073346749533],
-               [-76.4225396149832,715.693774717041,-52.9081045914678],
-               [27.8706194688158,736.259256448681,30.7476818991719],
-               [75.9266183557466,715.457396322932,-55.0604622597071],
-               [-97.2254670188798,-841.625806800333,-35.4809212169046],
-               [97.2881989464546,-841.626116231009,-35.4119624250308]]
+                     [-0.000732270938924443,175.158289701814,0.0000404],
+                     [0.104501423707752,350.306093180137,0.061023624087894],
+                     [0.209734388920577,525.459141360196,0.122005361332611],
+                     [-47.5920764358155,526.439401661188,0.945479045649915],
+                     [-173.508163386225,526.509589382348,2.98848444604651],
+                     [-413.490495136004,529.070006884027,4.30015451481757],
+                     [-644.259234923473,531.557805055099,5.5156119864156],
+                     [-690.412981869986,532.055366973068,5.75870846651414],
+                     [-782.7204827236,533.050484095638,6.24488492674942],
+                     [-737.171470366004,477.177940319642,6.03507728101326],
+                     [48.0126939292815,526.382576868867,-0.700813930486266],
+                     [173.928779031094,526.312389136708,-2.74381818341968],
+                     [413.965281719423,525.913766951398,-5.58751914341834],
+                     [644.770225962532,525.03114718918,-8.36370311424063],
+                     [690.931216942206,524.854620798345,-8.91894225928869],
+                     [783.253196286002,524.501573309859,-10.029413939978],
+                     [736.897241876494,469.296359031723,-9.22217198040476],
+                     [-97.2538992002065,0,-0.0216466824273884],
+                     [-97.2534603765872,-398.665678321646,-0.0280368622539885],
+                     [-97.236806268837,-753.034804644565,-0.0408179870540185],
+                     [-97.2541801180481,-841.630928132314,106.266711069826],
+                     [97.2538982249762,0,0.0216480069085377],
+                     [97.2606469820357,-398.664829041876,0.0265545153545871],
+                     [97.2769776133806,-753.033961966181,0.0252733646276821],
+                     [97.2596464463626,-841.626635633415,106.335670500348],
+                     [1.30731388292955,660.085767955841,62.6295143653575],
+                     [1.37823874062583,704.938429698234,62.5519366785149],
+                     [-25.8996591150869,736.342683178085,31.5073346749533],
+                     [-76.4225396149832,715.693774717041,-52.9081045914678],
+                     [27.8706194688158,736.259256448681,30.7476818991719],
+                     [75.9266183557466,715.457396322932,-55.0604622597071],
+                     [-97.2254670188798,-841.625806800333,-35.4809212169046],
+                     [97.2881989464546,-841.626116231009,-35.4119624250308]]
 
+# tpose34_pos = [[-a, b, c] for a, b, c in tpose34_pos_xflip]
 
 
 
@@ -309,8 +310,14 @@ class Quantized_Quaternion:
         floats.append(math.sqrt(1.0 - sum(sqrs)))
         return Quaternion(floats)
 
+    def zero():
+        return Quantized_Quaternion([0.0, 0.0, 0.0])
+
     def __str__(self):
         return Quaternion.toQuaternion.__str__()
+
+    def np(self):
+        return np.array(self.fixed).astype(np.int16)
     
 class Quaternion:
 
@@ -339,7 +346,9 @@ class Quaternion:
     def apply(self, x):
         return Position(self.rot.apply([x.x, x.y, x.z]))
 
-
+    def np(self):
+        return self.rot.as_quat()
+    
     def toQuantQuat(self):
         q = self.rot.as_quat()
         if (q[3] < 0):
@@ -363,21 +372,28 @@ class Euler:
     def __str__(self):
         return (" ".join([str(i) for i in [self.e0, self.e1, self.e2]]))
 
-    def toQuat(self, zneg = False):
+    def toQuat(self, xneg = False, zneg = False):
 
         rot = Rotation.from_euler(self.perm, [self.e0,
                                               self.e1,
                                               self.e2], degrees = True)
         q = rot.as_quat()
+
         
+        if (xneg == True):
+            xcoord = -q[0]
+        else:
+            xcoord = q[0]
         if (zneg == True):
-            return Quaternion([q[0], q[1], -q[2], q[3]])
+            zcoord = -q[2]
+        else:
+            zcoord = q[2]
 
-        return Quaternion(list(q))
+        return Quaternion([xcoord, q[1], zcoord, q[3]])
 
 
-    def toQuantQuat(self):
-        return self.toQuat().toQuantQuat()
+    def toQuantQuat(self, xneg = False, zneg = False):
+        return self.toQuat(xneg = xneg, zneg = zneg).toQuantQuat()
 
 class Position:
     def __init__(self, floats):
@@ -472,54 +488,114 @@ class ForwardKinematics:
         _recurse(self.root, initial_rot, -1)
         
         return keyvector
-        
-class BVHReader():
 
-    # Reads a BVH file in ZED signal format
-    
-    def __init__(self, filename):
+
+class BVHReader_NP():
+    def __init__(self, filename, noroot = False):
         self.filename = filename
         with open(self.filename, 'r') as fp:
             self.mocap = Bvh(fp.read())
 
-        self.rots = [self.get_quaternions(i) for i in range(self.mocap.nframes)]
+        self.noroot = noroot            
+        self.eulers = self.get_rotations()
+        
+    def get_rotations(self):
+        self.rots = np.zeros([self.mocap.nframes, 34, 3])
+        # self.rots[:, :, 3] = 1.0
+        # self.rots[:, body_parts34.index('LEFTHEEL'), 0] = math.sqrt(2)
+        # self.rots[:, body_parts34.index('LEFTHEEL'), 3] = math.sqrt(2)
+        # self.rots[:, body_parts34.index('RIGHTHEEL'), 0] = math.sqrt(2)
+        # self.rots[:, body_parts34.index('RIGHTHEEL'), 3] = math.sqrt(2)
 
 
-    def get_quantized_quats(self, frame):
+        for frame in range(self.mocap.nframes):
+            
+            for joint in self.mocap.get_joints_names():
+                jidx = body_parts34.index(joint.upper())
 
+
+                chan = [c for c in self.mocap.joint_channels(joint) if c[1:] == 'rotation']
+                
+                perm = "".join([c[0].lower() for c in chan if c[1:] == 'rotation'])
+                fval = self.mocap.frame_joint_channels(frame, joint, chan)
+                zindex = perm.index('z')
+                fval[zindex] = -fval[zindex]                
+                self.rots[frame, jidx, :] = fval
+
+        
+    
+    def get_quaternions(self, frame, quantized = False):
+        
         rotations = [Quaternion.zero() for i in range(34)]
+
+        # Weird feet hack. Why is this necessary?
+        rotations[body_parts34.index('LEFTHEEL')] = Quaternion([math.sqrt(2), 0, 0, -math.sqrt(2)])
+        rotations[body_parts34.index('RIGHTHEEL')] = Quaternion([math.sqrt(2), 0, 0, -math.sqrt(2)])                  
         
         for joint in self.mocap.get_joints_names():
             jidx = body_parts34.index(joint.upper())
-            
-            fval = self.mocap.get_frame_joint_channels(frame, joint, chan)
-            fval[2] = -fval[2]
-            #perm = "".join([c[0].lower() for c in self.mocap.joint_channels(joint) if c[1:] == 'rotation'])
-            perm = "".join([c[0].lower() for c in self.mocap.joint_channels(joint) if c[1:] == 'rotation'])
-            rotations[jidx] = Euler(fval, perm = perm).toQuantQuat()
-            
-        return rotations
-
-    def get_quaternions(self, frame):
-
-        rotations = [Quaternion.zero() for i in range(34)]
-        
-        for joint in self.mocap.get_joints_names():
-            jidx = body_parts34.index(joint.upper())
-            
-
             chan = self.mocap.joint_channels(joint)
 
             perm = "".join([c[0].lower() for c in chan if c[1:] == 'rotation'])
-            #fval = self.mocap.frame_joint_channels(frame, joint, ['Zrotation', 'Yrotation', 'Xrotation'])
-            fval = self.mocap.frame_joint_channels(frame, joint, chan)
+            rfval = self.mocap.frame_joint_channels(frame, joint, chan)
 
             zindex = perm.index('z')
+            fval = rfval.copy()
             fval[zindex] = -fval[zindex]
-            rotations[jidx] = Euler(fval, perm = perm).toQuat(zneg = True)
-            #print("Euler(%s) %s converted to %s"%(perm, Euler(fval, perm = perm), rotations[jidx]))
-            #print("Euler(%s) %s converted to %s"%(perm, fval, rotations[jidx]))
-            
+
+            if (quantized):
+                rotations[jidx] = Euler(fval, perm = perm).toQuantQuat(xneg = True, zneg = False)                
+                if (self.noroot):
+                    rotations[0] = Quantized_Quaternion.zero()
+            else:
+                rotations[jidx] = Euler(fval, perm = perm).toQuat(xneg = True, zneg = False)
+                if (self.noroot):
+                    rotations[0] = Quaternion.zero()
+
+        return rotations
+
+    
+class BVHReader():
+
+    # Reads a BVH file in ZED signal format
+    
+    def __init__(self, filename, noroot = False):
+        self.filename = filename
+        with open(self.filename, 'r') as fp:
+            self.mocap = Bvh(fp.read())
+
+        self.noroot = noroot            
+        self.rots = [self.get_quaternions(i) for i in range(self.mocap.nframes)]
+        self.skeltype = "Body_34_KeypointsPlus"
+
+    def get_quaternions(self, frame, quantized = False):
+
+        rotations = [Quaternion.zero() for i in range(34)]
+
+        # Weird feet hack. Why is this necessary?
+        rotations[body_parts34.index('LEFTHEEL')] = Quaternion([math.sqrt(2), 0, 0, math.sqrt(2)])
+        rotations[body_parts34.index('RIGHTHEEL')] = Quaternion([math.sqrt(2), 0, 0, math.sqrt(2)])                  
+        
+        for joint in self.mocap.get_joints_names():
+            jidx = body_parts34.index(joint.upper())
+            chan = self.mocap.joint_channels(joint)
+
+            perm = "".join([c[0].lower() for c in chan if c[1:] == 'rotation'])
+            rfval = self.mocap.frame_joint_channels(frame, joint, chan)
+
+            zindex = perm.index('z')
+            fval = rfval.copy()
+            fval[zindex] = -fval[zindex]
+
+            if (quantized):
+                rotations[jidx] = Euler(fval, perm = perm).toQuantQuat(xneg = True, zneg = False)                
+                if (self.noroot):
+                    rotations[0] = Quantized_Quaternion.zero()
+            else:
+                rotations[jidx] = Euler(fval, perm = perm).toQuat(xneg = True, zneg = False)
+                if (self.noroot):
+                    rotations[0] = Quaternion.zero()
+
         return rotations
 
     def get_keypoints(self):
@@ -527,35 +603,73 @@ class BVHReader():
         # Use the non-quantized rotations
         fk = ForwardKinematics(body_parts34, body_34_tree, 'PELVIS', tpose34_pos)
         print([str(Q) for Q in self.get_quaternions(4)])
-        kp = [fk.propagate(self.get_quaternions(frame), Position([0, 0, 0])) for frame in range(self.mocap.nframes)]
+        kp_ = [fk.propagate(self.get_quaternions(frame), Position([0, 0, 0])) for frame in range(self.mocap.nframes)]
+
+        kp = [[Position([k.x, k.y, k.z]) for k in l] for l in kp_]
+        
         return kp
 
-    def write_zed_frame_data(self, fp):
-        pass
+
+    def dump_data(self, filename = None):
+        if (filename is None):
+            if (self.filename [-4:] == '.bvh'):
+                new_filename = self.filename[:-4] + ".npz"
+            else:
+                new_filename = self.filename + ".npz"
+        else:
+            new_filename = filename
+                
+        timeval = np.array(self.mocap.frame_time)
+        skeltype = np.array(self.skeltype)
+        quants = np.array([[c.toQuantQuat().np() for c in r] for r in self.rots])
+        keypoints = np.array([[k.np() for k in l] for l in self.get_keypoints()])
+        fullquats = np.array([[c.np() for c in r] for r in self.rots])
+        np.savez(new_filename, skeltype = skeltype, frametime = timeval, quantized_quats = quants, quats = fullquats, keypoints = keypoints)
     
 
-    def write_zed_header(self, fp):
-        pass
-
-    def write_frame(self, fp, frame, time):
-        fp.write(struct.pack('i', DGSAV_FRAME_MARKER))
-        # write json header
-        # write signalmetadata
-        # write zed header
-        # write zed data 
-                 
     def write_header(self, fp):
         fp.write(DANCEGRAPH_SAVE_MARKER.encode('utf-8'))
         jsonbundle = json.dumps(DGS_HEADER_TEMPLATE, indent = 4).encode('utf-8')
         fp.write(struct.pack('i', len(jsonbundle)))
         fp.write(jsonbundle)
         
+
+    def write_frame_metadata(self, fp):
+        fp.write(struct.pack('i', DANCEGRAPH_FRAME_MARKER))
+
+        
+
+    # def write_zed_frame_data(self, fp):
+    #     pass
+    
+
+    # def write_zed_header(self, fp):
+    #     pass
+
+    # def write_zedbodies_header(self, fp):
+    #     pass
+
+    # def write_zedbodies_body(self, fp):
+    #     pass
+
+
+    
+    # def write_frame(self, fp, frame, time):
+
+
+                 
     
     def write_zed(self, filename):
+        
         with open(filename, 'wb') as fp:
-            self.write_header(fp)
+
+            self.write_signal_header(fp)
+            time = 0
+
             for i in range(len(self.rotations)):
+                self.write_frame_metadata(fp, time)
                 self.write_frame(self, fp, i)
+                
 
 
 class AnimatedScatterKP:
@@ -650,15 +764,14 @@ if (__name__ == '__main__'):
             pass
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--norootrot", action = 'store_true')
     parser.add_argument("--savefile", type = str, default = None)
     parser.add_argument("infile", type = str)
     args = parser.parse_args()
 
-    bvh = BVHReader(args.infile)
-    kp = bvh.get_keypoints()
-
-    # kpz = np.array([v.np() for v in kp[0]])
-    # np.save("Keypoints0.npy", kpz)
-    #npkp = np.array([[b.np() for b in frame] for frame in kp])
-
-    anim = AnimatedScatterKP(kp, skellines = True, savefile = args.savefile, fps = 1.0 / bvh.mocap.frame_time)
+    bvh = BVHReader(args.infile, noroot = args.norootrot)
+    if (args.savefile):
+        bvh.dump_data(args.savefile)
+    else:
+        kp = bvh.get_keypoints()
+        anim = AnimatedScatterKP(kp, skellines = True, savefile = args.savefile, fps = 1.0 / bvh.mocap.frame_time)
